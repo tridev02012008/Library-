3cbd041549a8e013baa64110a42a941539ca3dfb
TRIDEV <tridev2460@gmail.com>
2026-09-20 16:32:25 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE='hariom-library-v12-1';
+const CORE=['./','./index.html','./manifest.json','./offline.html','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./offline.html'))));
+});
